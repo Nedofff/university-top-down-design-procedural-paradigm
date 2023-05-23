6c0e1759c1067abb75e574564f2e46ddbb3067eb
Eduard Novikov <edyard.novikov.d@gmail.com>
2023-05-23 14:04:52 +0700

    итерация2 - прописать основные функции, добавить вспомогательные в виде заглушек

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-
 # Задача: 
 # Туристическое агенство советует выбрать некоторое место(степень комфорта, место на карте).
 # Турист выбирает место с лучшим комфортом и отправляется.
 
 
+def get_travel_agency_tips(n):
+    pass
+
+def get_tourist_choice(tips):
+    pass
+
 
 def main():
-    pass
+
+    travel_agency_tips = get_travel_agency_tips()
+    print(get_tourist_choice(travel_agency_tips))
 
 
 if __name__ == '__main__':
